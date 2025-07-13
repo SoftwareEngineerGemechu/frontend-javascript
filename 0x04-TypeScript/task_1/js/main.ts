6d1f5d79bf48adf0458d379445c737906bd1758b
SoftwareEngineerGemechu <gemechu.bekele.berga@gmail.com>
2025-07-13 21:58:11 +0000

    Add task_1: Teacher interface with config and clean files

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+// Define the Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allow additional properties
+}
+
+// Create a teacher object with extra attribute
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
